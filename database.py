b988aabdbdad373dfe19c307ad13da5de3ff69fb
Greeny187 <greeny187@outlook.de>
2025-06-17 14:58:50 +0200

    Update database.py

--- database.py
+++ database.py
@@ -36,6 +36,7 @@ def init_db():
                 daily_stats_enabled BOOLEAN NOT NULL DEFAULT TRUE,
                 rss_topic_id BIGINT NOT NULL DEFAULT 0,
                 mood_question TEXT NOT NULL DEFAULT 'Wie fühlst du dich heute?'
+            );
         """)
         cur.execute("""
             CREATE TABLE IF NOT EXISTS welcome (
