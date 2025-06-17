021309da05e09ac9ec84024fc3a4f56b5ec9f4bb
Greeny187 <greeny187@outlook.de>
2025-06-17 07:47:43 +0200

    Update database.py

--- database.py
+++ database.py
@@ -75,6 +75,7 @@ def init_db():
             CREATE TABLE IF NOT EXISTS user_topics (
                 chat_id BIGINT,
                 user_id BIGINT,
+                topic_id BIGINT,
                 PRIMARY KEY (chat_id, user_id)
             );
         """)
