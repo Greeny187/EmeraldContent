d7d8b1c06ffac651af35fb2fd554b8a032bb77b3
Greeny187 <greeny187@outlook.de>
2025-06-20 21:59:10 +0200

    Update handlers.py

--- handlers.py
+++ handlers.py
@@ -85,7 +85,6 @@ async def message_logger(update, context):
             logger.info(f"➕ add_member via message_logger: chat={msg.chat.id}, user={msg.from_user.id}")
         except Exception as e:
             logger.info(f"Fehler add_member in message_logger: {e}", exc_info=True)
-    await text_handler(update, context)
 
 async def text_handler(update: Update, context: ContextTypes.DEFAULT_TYPE):
     chat = update.effective_chat
