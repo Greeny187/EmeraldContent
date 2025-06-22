ce364dcbcbf1c863ce301ea46a0d07ff050404ad
Greeny187 <greeny187@outlook.de>
2025-06-22 19:58:07 +0200

    Update bot.py

--- bot.py
+++ bot.py
@@ -56,6 +56,6 @@ if __name__ == "__main__":
     app.run_webhook(
         listen="0.0.0.0",
         port=PORT,
-        url_path="/webhook",
+        url_path="webhook",
         webhook_url=WEBHOOK_URL,
     )
