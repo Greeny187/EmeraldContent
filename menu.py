49288b764d61e0834d7d95f56a2e04fb1b60943b
Greeny187 <greeny187@outlook.de>
2025-08-17 01:18:28 +0200

    Update menu.py

--- menu.py
+++ menu.py
@@ -954,7 +954,7 @@ def register_menu(app):
     app.add_handler(CallbackQueryHandler(menu_callback))
     app.add_handler(MessageHandler(
         filters.REPLY
-        & (filters.TEXT | filters.PHOTO | filters.document)
+        & (filters.TEXT | filters.ATTACHMENT)  # erfasst alle Medienarten
         & filters.ChatType.GROUPS,
         menu_free_text_handler
     ), group=1)
